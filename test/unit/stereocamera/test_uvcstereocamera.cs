e43fba655c8fc57b0f02c7e286f97c027264a7ae
Ben <benknight135@gmail.com>
2022-08-26 12:02:38 +0000

    add camera unit test

--- test/unit/stereocamera/test_uvcstereocamera.cs
+++ test/unit/stereocamera/test_uvcstereocamera.cs
@@ -21,14 +21,56 @@ namespace I3DR.PhaseTest
         public void test_GettingSettingParams()
         {
             // Test stereo camera parameters can be set and get functions return expected values
-            // TOTEST
+            CameraDeviceInfo device_info = new CameraDeviceInfo(
+                "0", "0", "virtual-camera",
+                CameraDeviceType.DEVICE_TYPE_GENERIC_UVC,
+                CameraInterfaceType.INTERFACE_TYPE_VIRTUAL
+            );
+            UVCStereoCamera cam = (UVCStereoCamera) StereoCamera.createStereoCamera(device_info);
+
+            // int frame_rate = 5;
+            // int exposure = 500;
+            // bool hardware_trigger = false;
+            // int x_min = 10;
+            // int x_max = 100;
+            // int y_min = 10;
+            // int y_max = 100;
+
+            // TODO fix setting exposure for UVC Cameras
+            // cam->setExposure(500);
+            // TODO fix setting hardware trigger for UVC Cameras
+            // cam->enableHardwareTrigger(false);
+            // TODO fix setting frame rate for UVC Cameras
+            // cam->setFrameRate(frame_rate);
+            // TODO fix setting AOI for virutal camera
+            // cam->setLeftAOI(x_min, x_max, y_min, y_max);
+            // cam->setRightAOI(x_min, x_max, y_min, y_max);
+
+            // TODO add get methods to DeimosStereoCamera to check values have been set
+
+            // TODO fix getting frame rate for virtual camera
+            // // read a few frames as frame rate is calculated
+            // cam->startCapture();
+            // cam->read();
+            // cam->read();
+            // REQUIRE(cam->getFrameRate() == frame_rate);
         }
 
         [Fact]
         public void test_VirtualCanConnect()
         {
             // Test virtual camera can be connected to using ‘connected’ function
-            // TOTEST
+            CameraDeviceInfo device_info = new CameraDeviceInfo(
+                "0", "0", "virtual-camera",
+                CameraDeviceType.DEVICE_TYPE_GENERIC_UVC,
+                CameraInterfaceType.INTERFACE_TYPE_VIRTUAL
+            );
+            UVCStereoCamera cam = (UVCStereoCamera) StereoCamera.createStereoCamera(device_info);
+            string data_folder = "data";
+            string left_image_file = data_folder + "/left.png";
+            string right_image_file = data_folder + "/right.png";
+            cam.setTestImagePaths(left_image_file, right_image_file);
+            Assert.True(cam.connect());
         }
 
         [Fact]
@@ -36,7 +78,21 @@ namespace I3DR.PhaseTest
         {
             // Test read frame from virtual camera using ‘read’ function
             // and valid read result is returned
-            // TOTEST
+            CameraDeviceInfo device_info = new CameraDeviceInfo(
+                "0", "0", "virtual-camera",
+                CameraDeviceType.DEVICE_TYPE_GENERIC_UVC,
+                CameraInterfaceType.INTERFACE_TYPE_VIRTUAL
+            );
+            UVCStereoCamera cam = (UVCStereoCamera) StereoCamera.createStereoCamera(device_info);
+            string data_folder = "data";
+            string left_image_file = data_folder + "/left.png";
+            string right_image_file = data_folder + "/right.png";
+            cam.setTestImagePaths(left_image_file, right_image_file);
+            bool connected = cam.connect();
+            Assert.True(connected);
+            cam.startCapture();
+            CameraReadResult read_result = cam.read();
+            Assert.True(read_result.valid);
         }
 
         [Fact]
